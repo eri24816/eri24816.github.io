af69dea0d216c42c0f901cb38c107baf670de614
eri24816 <a931eric@gmail.com>
2022-05-05 18:46:01 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,10 @@ hugo
 git add .
 git commit -m "update"
 git push
+
+cd .\themes\chunky-poster\
+git add .
+git commit -m "update"
+git push git@github.com:eri24816/hugo-theme-chunky-poster.git master
+
 start chrome "https://eri24816.github.io/"
