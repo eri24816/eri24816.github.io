ced07393942a005dffc33cac0c2ee2e1cc9ef922
eri24816 <a931eric@gmail.com>
2023-09-11 17:59:03 +0800

    update

--- content/posts/contact.md
+++ content/posts/contact.md
@@ -1,1 +1,13 @@
-eri24816@gmail.com
+---
+title: "Contact"
+date: 2023-09-11T15:21:01+08:00
+draft: false
+categories: "self"
+summary: "Contact me"
+---
+
+Mail: eri24816@gmail.com
+
+Twitter: [eri24816](https://twitter.com/eri24816)
+ 
+Instagram: [eri24816](https://www.instagram.com/eri24816/)
--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,13 @@
 hugo
-git add .
-git commit -m "update"
-git push
 
 cd .\themes\chunky-poster\
 git add .
 git commit -m "update"
 git pull git@github.com:eri24816/hugo-theme-chunky-poster.git master
 git push git@github.com:eri24816/hugo-theme-chunky-poster.git master
+
+cd ..\..
+
+git add .
+git commit -m "update"
+git push
